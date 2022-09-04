6e3679074c42562f2cd6a3216bd66a5ddfe004d6
sreddevil123123 <sreddevil2@gmail.com>
2022-09-04 18:31:44 +0545

    #3 till orders data table

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Orders from "./pages/Orders";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Login />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="orders" element={<Orders />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
